7d6198859ac5518fe3fa975694da2d525aee203d
navyblue1993 <john245678@gmail.com>
2023-04-11 19:25:15 -0400

    include iris

--- src/datasets/dataset_utils.py
+++ src/datasets/dataset_utils.py
@@ -1,9 +1,12 @@
 from .mnist import *
+from .iris import load_iris
 
 def get_dataset(dataset = "mnist", seed = 15618, compress_data = True, filepath = None):
     print(f'Loading dataset [{dataset}]')
     if dataset == "mnist":
         return load_mnist(seed=seed, compress_data=compress_data)
+    elif dataset == "iris":
+        return load_iris(seed=seed, compress_data=compress_data)
     else:
         print(f'Dataset: [{dataset}] not support')
         return None
