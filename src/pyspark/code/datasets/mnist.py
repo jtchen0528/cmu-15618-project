00659b3de6d4312addbd5704e172f798744e5844
Jhao-Ting Chen <jtchen0528@gmail.com>
2023-04-16 01:37:04 -0400

    finished centroids initialization

--- src/pyspark/code/datasets/mnist.py
+++ src/pyspark/code/datasets/mnist.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-def load_mnist(path = "data/mnist.npz", seed = 15618, compress_data = False):
+def load_mnist(path = "../../data/mnist.npz", seed = 15618, compress_data = False):
     with np.load(path, allow_pickle=True) as f:
         x_train, y_train = f["x_train"], f["y_train"]
         x_test, y_test = f["x_test"], f["y_test"]
