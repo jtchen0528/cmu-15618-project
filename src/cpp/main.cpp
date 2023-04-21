07854fad8ee613a0a0e1f3c27add5b485d68743e
navyblue1993 <john245678@gmail.com>
2023-04-20 20:58:42 -0400

    naive DBSCAN impl.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -3,8 +3,82 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <memory>
 
-std::vector<std::vector<double>> parse(std::string filename) 
+class Point {
+public:
+    Point(const int _label, const std::vector<double> vd) : label{_label}, coords{vd} {};
+    int label;
+    std::vector<double> coords;
+};
+
+double euclideanDistance(Point p, Point q) {
+    double distance = 0.0;
+    for (int i = 0; i < p.coords.size(); i++) {
+        distance += pow(p.coords[i] - q.coords[i], 2);
+    }
+    return sqrt(distance);
+}
+
+std::vector<int> rangeQuery(std::vector<Point>& points, int p, double eps) {
+    std::vector<int> neighbors;
+    for (int i = 0; i < points.size(); i++) {
+        if (euclideanDistance(points[p], points[i]) <= eps) {
+            neighbors.push_back(i);
+        }
+    }
+    return neighbors;
+}
+
+
+std::vector<int> dbscan(std::vector<Point>& points, double eps, int minPts) {
+    std::vector<int> visited(points.size(), 0);
+    std::vector<int> cluster(points.size(), -1);
+    int clusterIdx = 0;
+
+    for (int i = 0; i < points.size(); i++) {
+        if (visited[i] == 1) continue;
+
+        visited[i] = 1;
+        std::vector<int> neighbors = rangeQuery(points, i, eps);
+        if (neighbors.size() < minPts) {
+            cluster[i] = -1;
+        } else {
+            cluster[i] = clusterIdx;
+            for (int j = 0; j < neighbors.size(); j++) {
+                int idx = neighbors[j];
+                if (visited[idx] == 0) {
+                    visited[idx] = 1;
+                    std::vector<int> subNeighbors = rangeQuery(points, idx, eps);
+                    if (subNeighbors.size() >= minPts) {
+                        neighbors.insert(neighbors.end(), subNeighbors.begin(), subNeighbors.end());
+                    }
+                }
+                if (cluster[idx] == -1) {
+                    cluster[idx] = clusterIdx;
+                }
+            }
+            clusterIdx++;
+        }
+    }
+
+    std::cout << "Clusters: " << clusterIdx << std::endl;
+    for (int i = 0; i < points.size(); i++) {
+        std::cout << "(";
+        for (int j = 0; j < points[i].coords.size(); j++) {
+            std::cout << points[i].coords[j];
+            if (j < points[i].coords.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "): " << cluster[i] << std::endl;
+    }
+
+    return cluster;
+}
+
+std::vector<Point> parse(std::string filename) 
 {
     std::ifstream infile(filename);
     if (!infile.is_open()) {
@@ -12,7 +86,7 @@ std::vector<std::vector<double>> parse(std::string filename)
     }
 
     std::string line{};
-    std::vector<std::vector<double>> vvd{};
+    std::vector<Point> vupp{};
     while (std::getline(infile, line)) {
         std::istringstream iss(line);
         std::vector<double> values{};
@@ -27,19 +101,19 @@ std::vector<std::vector<double>> parse(std::string filename)
             }
             values.push_back(value);
         }
-        // Do something with the values vector here
-        for (auto value : values) {
-            std::cout << value << " ";
-        }
-        vvd.push_back(values);
+        int label = static_cast<int>(values.back());
+        values.pop_back();
+        Point p = Point(label, values);
+        vupp.push_back(p);
         std::cout << "\n";
     }
 
     infile.close();
-    return vvd;
+    return vupp;
 }
 
 int main() {
-    auto metadata(parse("iris_dataset.csv"));
+    auto points(parse("iris_dataset.csv"));
+    auto clusters(dbscan(points, 0.5, 5));
     return 0;
 }
